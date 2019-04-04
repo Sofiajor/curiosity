ec0dae735b0414c06349127c8d2bf1919c9fc30f
Sofiajor <sofia.jordan2000@gmail.com>
2019-04-04 20:23:27 +0200

    ready for deploy

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ require('./configs/passport')
 
 
 mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .connect(`${process.env.DB}`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
