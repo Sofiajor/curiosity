9ec8c9e9712341ec77141c324f73b600141affe4
Sofiajor <sofia.jordan2000@gmail.com>
2019-04-04 20:16:22 +0200

    ready for deploy

---
+++ server/public/precache-manifest.34195206a647d9e07260706d53a3edb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "566b9dc3c5e0c94ad3ef",
+    "url": "/static/css/main.84edc2d0.chunk.css"
+  },
+  {
+    "revision": "566b9dc3c5e0c94ad3ef",
+    "url": "/static/js/main.8d820461.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be88d1f61685e6e9820f",
+    "url": "/static/js/2.9dcfbf8f.chunk.js"
+  },
+  {
+    "revision": "29222aa75da5656ed4e8617f91f21571",
+    "url": "/static/media/ironhack1.29222aa7.jpg"
+  },
+  {
+    "revision": "9cd5d4c543cecba635eee470104a546f",
+    "url": "/static/media/plant.9cd5d4c5.jpg"
+  },
+  {
+    "revision": "24cbf2fd92c120803b59b44786a6c684",
+    "url": "/index.html"
+  }
+];
